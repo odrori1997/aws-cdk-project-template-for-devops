6a98635f00a5a5609a276b7b9ebb336a096f9211
Omer Drori <odrori574@gmail.com>
2025-11-18 12:55:26 -0500

    Fixes build issues upgrades test script

--- test_script.sh
+++ test_script.sh
@@ -2,3 +2,11 @@ git add .
 git commit -m "test: ec2 upgrade"
 git checkout -b test-ec2-upgrade
 git push origin test-ec2-upgrade
+
+# Create PR from test-ec2-upgrade to main
+gh pr create \
+  --base main \
+  --head test-ec2-upgrade \
+  --repo odrori1997/aws-cdk-project-template-for-devops \
+  --title "test: ec2 upgrade" \
+  --body "Test PR for EC2 upgrade changes"
