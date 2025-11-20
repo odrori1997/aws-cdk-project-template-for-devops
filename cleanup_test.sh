edeb609bfb00261e27857c8882196947d7bbc670
Omer Drori <odrori574@gmail.com>
2025-11-20 14:11:11 -0500

    Cleanup test script

---
+++ cleanup_test.sh
@@ -0,0 +1,11 @@
+# Close any open PRs for test-ec2-upgrade branch
+gh pr list --head test-ec2-upgrade --repo odrori1997/aws-cdk-project-template-for-devops --json number --jq '.[].number' | while read pr_number; do
+  if [ ! -z "$pr_number" ]; then
+    echo "Closing PR #$pr_number"
+    gh pr close "$pr_number" --repo odrori1997/aws-cdk-project-template-for-devops
+  fi
+done
+
+git checkout main
+git branch -D test-ec2-upgrade
+git reset HEAD~1
